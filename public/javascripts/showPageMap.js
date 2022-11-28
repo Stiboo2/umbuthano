029b39ecceb88a79c2a965c1e1a4112e82fe7a28
Thabo <thabo.radebe@live.com>
2022-11-28 06:11:31 +0200

    map folder and create space on show page ref:549

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,18 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+    center: inyanga.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10 // starting zoom
+});
+
+/* new mapboxgl.Marker()
+    .setLngLat(inyanga.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${inyanga.title}</h3><p>${inyanga.location}</p>`
+            )
+    )
+    .addTo(map)
+ */
